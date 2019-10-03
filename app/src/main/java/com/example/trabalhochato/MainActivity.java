3928184ba6859fa81262267c6d104f482b72f89f
Wesley Guedes <tairoria@gmail.com>
2019-10-03 20:29:54 -0300

    ODIO PURO

--- app/src/main/java/com/example/trabalhochato/MainActivity.java
+++ app/src/main/java/com/example/trabalhochato/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private Calculus yea = new Calculus();
+
+
     private final int REQ_CODE_SPEAKER_INPUT = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
 
         //
         super.onActivityResult(requestCode, resultCode, data);
+        TextView potencia = findViewById(R.id.p);
+        TextView resistencia = findViewById(R.id.r);
+        TextView corrente = findViewById(R.id.c);
+        TextView tensao = findViewById(R.id.t);
 
         switch (requestCode) {
 
@@ -69,36 +76,52 @@ public class MainActivity extends AppCompatActivity {
                     //mostrar a mensagem
 
                     while (true) {
+                        String mensagem = "";
                         Log.i("Falar o calculo", mensagem);
                     int porn = 0;
                     String[] dammit = result.get(0).split(" ");
                         for (int i = 0; porn < 2 && i < dammit.length; i++) {
-                            switch (dammit[i].toLowerCase()){ case "potência": {
+                            switch (dammit[i].toLowerCase()){
+                                case "potência": {
                                 if(porn<2){
                                 Double valor = Double.parseDouble(dammit[i++]);
                                 yea.setPotencia(valor);
+                                potencia.setText(valor.toString());
                                 porn++;
                             }break;}
                             case "tensão": {
                                 if(porn<2){
                                 Double valor = Double.parseDouble(dammit[i++]);
                                 yea.setTensao(valor);
+                                tensao.setText(valor.toString());
                                 porn++;
                             }break;}
                             case "corrente": {
                                 if(porn<2){
                                 Double valor = Double.parseDouble(dammit[i++]);
                                 yea.setCorrente(valor);
+                                corrente.setText(valor.toString());
                                 porn++;
                             }break;}
                             case "resistência": {
                                 if(porn<2){
                                 Double valor = Double.parseDouble(dammit[i++]);
                                 yea.setResistencia(valor);
+                                resistencia.setText(valor.toString());
                                 porn++;
                             }break;}
-                            case "calcular": {
-                            if(porn==2){}
+                            case "calcullar": {
+                            if(porn==2){
+                                Double pot = yea.getPotencia();
+                                Double res = yea.getResistencia();
+                                Double cor = yea.getCorrente();
+                                Double ten = yea.getTensao();
+
+                                potencia.setText(String.format(pot.toString()));
+                                resistencia.setText(String.format(res.toString()));
+                                corrente.setText(String.format(pot.toString()));
+                                tensao.setText(String.format(ten.toString()));
+                            }
                             else{yea.reset();}
                             }}
                         }
@@ -107,16 +130,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-
-
-        public void Falar(View view) {
-
-
-            promptSpeechInput();
-
-
-        }
-
     }
 
+    public void Falar(View view) {
+        promptSpeechInput();
+    }
 }
